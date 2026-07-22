c3a253a87fd9396bbf71b4baf25fcbde11b53630
ahmed-rana <ahmed-rana@users.noreply.github.com>
2026-07-23 02:26:16 +0500

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/DimeRepublic-website/',
   server: {
     port: 5173,
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
   },
 });
