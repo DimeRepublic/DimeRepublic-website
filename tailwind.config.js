b547b88ef94209134b6f65b9318d8f509040f60a
ahmed-rana <ahmed-rana@users.noreply.github.com>
2026-07-23 04:49:15 +0500

    Apply case study typography: Fraunces+Inter+IBM Plex Mono, updated sizes, paper bg, white panels

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ export default {
     extend: {
       colors: {
         navy: {
-          DEFAULT: '#101A34',
-          50: '#EEF1F7',
-          100: '#D8DFEE',
-          400: '#3D4C77',
+          DEFAULT: '#0F2438',
+          50: '#F5F7F5',
+          100: '#EEF1F7',
+          400: '#445064',
           600: '#1B2A52',
           700: '#131F40',
           800: '#0F1930',
@@ -25,10 +25,17 @@ export default {
           50: '#F6E9D2',
           600: '#C4923A',
         },
+        line: '#DEE4E1',
       },
       fontFamily: {
         display: ['"Fraunces"', 'serif'],
         sans: ['"Inter"', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+      fontSize: {
+        'display-1': ['44px', { lineHeight: '1.12', letterSpacing: '-0.01em', fontWeight: '600' }],
+        'display-2': ['26px', { lineHeight: '1.3', letterSpacing: '-0.01em', fontWeight: '600' }],
+        'display-3': ['22px', { lineHeight: '1.3', letterSpacing: '-0.01em', fontWeight: '600' }],
       },
       boxShadow: {
         card: '0 4px 24px -6px rgba(16, 26, 52, 0.08)',
